f5ce04819443c5e5330520057f3c15aea7ae054b
Brandon Marrero <brandon6190@outlook.com>
2018-05-02 20:25:53 -0400

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var cart = [];
+
+var product = {
+	name: "Apple",
+	price: 1.99,
+	unit: 3,
+}
+
+cart.push(product);
+
+console.log(cart);
+console.log(cart[0]);
+console.log(cart[0].name);
+console.log(cart[0].price);
+console.log(cart)
