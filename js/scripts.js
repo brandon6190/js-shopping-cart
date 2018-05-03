f50738c9b630a99b420059e942daf309e1547ca0
Brandon Marrero <brandon6190@outlook.com>
2018-05-02 20:36:26 -0400

    Added second product

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 var cart = [];
 
-var product = {
+var product1 = {
 	name: "Apple",
 	price: 1.99,
-	unit: 3,
+	unit: 3
 }
 
-cart.push(product);
+var product2 = {
+	name: "iPhone",
+	price: 399.99,
+	unit: 2
+}
+
+cart.push(product1);
+cart.push(product2);
 
 console.log(cart);
 console.log(cart[0]);
-console.log(cart[0].name);
-console.log(cart[0].price);
-console.log(cart)
+console.log(cart[1]);
