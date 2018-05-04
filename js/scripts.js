cecbe4e59ff3751e9d75310d3d635ff0c417ed93
Brandon Marrero <brandon6190@outlook.com>
2018-05-03 21:34:33 -0400

    Added functions to add and remove items from cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,55 @@
 var cart = [];
 
-var product1 = {
-	name: "Apple",
-	price: 1.99,
-	unit: 3
+var Item = function(name, price, count) {
+	this.name = name;
+	this.price = price;
+	this.count = count;
+};
+
+function addItemToCart(name, price, count) {
+	for (var i in cart) {
+		if (cart[i].name === name) {
+			cart[i].count += count;
+			return;
+		}
+	}
+	var item = new Item(name, price, count);
+	cart.push(item);
 }
 
-var product2 = {
-	name: "iPhone",
-	price: 399.99,
-	unit: 2
-}
+addItemToCart("Apple", 1.22, 1);
+addItemToCart("Pear", 1.99, 3);
+addItemToCart("Apple", 1.22, 2);
+addItemToCart("Apple", 1.22, 2);
 
-cart.push(product1);
-cart.push(product2);
+
+function removeItemFromCart(name) { // Removes one item
+	for (var i in cart) {
+		if (cart[i].name === name) {
+			cart[i].count --;
+			if (cart[i].count === 0) {
+				cart.splice(i, 1);
+			}
+			break;
+		}
+	}
+}
+console.log(cart);
+
+console.log(cart[0].count);
 
 console.log(cart);
-console.log(cart[0]);
-console.log(cart[1]);
+
+// removeItemFromCartAll(name) // Removes all items name
+
+// clearCart()
+
+// countCart() -> return total count
+
+// totalCart() -> return total cost
+
+// listCart() -> array of Item
+
+// saveCart()
+
+// loadCart()
